23a1351185c6494573e31f553d8a3a6c0f016058
Aslan Khetagurov <aslankhetagurovv@gmail.com>
2025-04-16 18:30:42 +0300

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+
+export default tseslint.config(
+    { ignores: ['dist', '**/*.d.ts'] },
+    {
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommendedTypeChecked,
+            tseslint.configs.stylisticTypeChecked,
+        ],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            globals: globals.browser,
+        },
+        parserOptions: {
+            project: ['./tsconfig.node.json', './tsconfig.app.json'],
+            tsconfigRootDir: import.meta.dirname,
+        },
+        settings: { react: { version: 'detect' } },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            react,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            ...react.configs['jsx-runtime'].rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            quotes: ['warn', 'single'],
+            semi: ['error', 'always'],
+            '@typescript-eslint/no-unused-vars': 'warn',
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/explicit-function-return-type': 'warn',
+        },
+    }
+);
