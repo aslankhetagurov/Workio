de75272718e49f3d34735fed8d76e315becdebb0
Aslan Khetagurov <aslankhetagurovv@gmail.com>
2025-04-29 01:22:40 +0300

    Add authSlice with toggleAuthModal and setUserData reducers

---
+++ src/modules/Auth/store/authSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+import { RootState } from '@/store/index';
+import { Tables } from '@/types/database.types';
+
+interface IAuthModalState {
+    userData: Tables<'users'> | null;
+    authModalIsOpen: boolean;
+}
+
+const initialState: IAuthModalState = {
+    userData: null,
+    authModalIsOpen: false,
+};
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        toggleAuthModal: (state) => {
+            state.authModalIsOpen = !state.authModalIsOpen;
+        },
+        setUserData: (state, action: PayloadAction<Tables<'users'> | null>) => {
+            state.userData = action.payload;
+        },
+    },
+});
+
+export const { setUserData, toggleAuthModal } = authSlice.actions;
+
+export const selectAuthModalIsOpen = (state: RootState) =>
+    state.authReducer.authModalIsOpen;
+export const selectUserData = (state: RootState) => state.authReducer.userData;
+
+export const authReducer = authSlice.reducer;
