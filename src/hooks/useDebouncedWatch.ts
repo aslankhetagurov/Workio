f4762633526559116f687d9fed166feb7492d223
Aslan Khetagurov <aslankhetagurovv@gmail.com>
2025-05-12 00:37:34 +0300

    Add useDebouncedWatch hook for input debounce

---
+++ src/hooks/useDebouncedWatch.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+export const useDebouncedWatch = <T extends string>(
+    value: T,
+    delay: number
+): T => {
+    const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setDebouncedValue(value);
+        }, delay);
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [value, delay]);
+
+    return debouncedValue;
+};
