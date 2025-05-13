7f257c5ff493da5dbd4bf2abda86a18a4f13cfdb
Aslan Khetagurov <aslankhetagurovv@gmail.com>
2025-05-13 03:20:40 +0300

    Integrate HeroSection module into home page

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import { HeroSection } from '@/modules/HeroSection';
+
+const HomePage = () => {
+    return (
+        <div>
+            <HeroSection />
+        </div>
+    );
+};
+
+export default HomePage;
