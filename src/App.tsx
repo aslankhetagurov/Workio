c772f25f5cb9e1ab96fa145215e391bc498beb00
Aslan Khetagurov <aslankhetagurovv@gmail.com>
2025-05-13 03:24:38 +0300

    Add HomePage with HeroSection module to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Header } from './modules/Header';
 import { Footer } from './modules/Footer';
 import { useInitAuth } from './modules/Auth/hooks/useInitAuth';
 import { AuthModal } from './modules/Auth';
+import HomePage from './pages/HomePage/HomePage';
 import './App.scss';
 
 function App() {
@@ -16,7 +17,9 @@ function App() {
                 <Toaster position="top-left" closeButton richColors />
                 <AuthModal />
                 <Header />
-                <main className="main"></main>
+                <main className="main">
+                    <HomePage />
+                </main>
                 <Footer />
             </div>
         </BrowserRouter>
