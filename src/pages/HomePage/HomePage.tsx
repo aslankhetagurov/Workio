2f7f4f41b7340349feb2ee61e05a4efd8eb8af7f
Aslan Khetagurov <aslankhetagurovv@gmail.com>
2025-05-14 17:01:38 +0300

    Integrate FeaturedVacancies module into home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from '@/modules/HeroSection';
+import { FeaturedVacancies } from '@/modules/FeaturedVacancies';
 
 const HomePage = () => {
     return (
         <div>
             <HeroSection />
+            <FeaturedVacancies />
         </div>
     );
 };
