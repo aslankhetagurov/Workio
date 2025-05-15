9549bb34662a2ae82d1527593a6d9a43e77533d0
Aslan Khetagurov <aslankhetagurovv@gmail.com>
2025-05-15 15:30:54 +0300

    Integrate PromoSection module into home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,13 @@
 import { HeroSection } from '@/modules/HeroSection';
 import { FeaturedVacancies } from '@/modules/FeaturedVacancies';
+import { PromoSection } from '@/modules/PromoSection';
 
 const HomePage = () => {
     return (
         <div>
             <HeroSection />
             <FeaturedVacancies />
+            <PromoSection />
         </div>
     );
 };
