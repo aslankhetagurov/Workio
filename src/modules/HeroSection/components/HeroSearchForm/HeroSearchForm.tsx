cb5b6751f992fdb209d3eadca2092cdcdd350080
Aslan Khetagurov <aslankhetagurovv@gmail.com>
2025-06-03 02:21:04 +0300

    Refactor HeroSearchForm to use LocationInput component

--- src/modules/HeroSection/components/HeroSearchForm/HeroSearchForm.tsx
+++ src/modules/HeroSection/components/HeroSearchForm/HeroSearchForm.tsx
@@ -1,16 +1,10 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { IoIosSearch, IoIosArrowDown, IoIosPin } from 'react-icons/io';
+import { IoIosSearch, IoIosArrowDown } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
 
 import PrimaryButton from '@/shared/UI/PrimaryButton/PrimaryButton';
-import { useDebouncedWatch } from '@/shared/hooks/useDebouncedWatch';
-import { TProfessionsType } from '@/shared/consts/professions';
-import {
-    ILocationItem,
-    TLocationApiResponse,
-    useGetLocationsQuery,
-} from '@/store/api/locationApi';
 import ProfessionInput from '@/shared/UI/ProfessionInput/ProfessionInput';
+import LocationInput from '@/shared/components/LocationInput/LocationInput';
 import styles from './HeroSearchForm.module.scss';
 
 interface IHeroSearchForm {
@@ -38,59 +32,7 @@ const HeroSearchForm = () => {
         navigate(`/${select.toLowerCase()}?${params.toString()}`);
     };
 
-    const locationInput = watch('locationInput');
     const select = watch('select');
-    const debouncedLocation = useDebouncedWatch(locationInput, 300);
-
-    const { data: locationList } = useGetLocationsQuery(debouncedLocation, {
-        skip: !debouncedLocation,
-    });
-
-    const isEquallyLocation =
-        !!locationList?.length && debouncedLocation
-            ? locationList[0].display_place.toLowerCase().trim() ===
-              debouncedLocation.toLowerCase().trim()
-            : null;
-
-    const handleSetValue = (field: THeroSearchFormKeys, item: string) => {
-        setValue(field, item);
-    };
-
-    const renderDropDownItems = (
-        items: TProfessionsType[] | TLocationApiResponse
-    ) => {
-        return (
-            <ul className={styles['form__dropdown-list']} role="listbox">
-                {items
-                    .slice(0, 5)
-                    .map((item: TProfessionsType | ILocationItem, i) => {
-                        const itemValue =
-                            typeof item === 'string'
-                                ? item
-                                : item.display_place;
-                        const field =
-                            typeof item === 'string'
-                                ? 'jobOrCompanyInput'
-                                : 'locationInput';
-                        return (
-                            <li
-                                className={styles['form__dropdown-item']}
-                                key={itemValue + i}
-                                role="option"
-                            >
-                                <PrimaryButton
-                                    label={itemValue}
-                                    ariaLabel={itemValue}
-                                    onClick={() =>
-                                        handleSetValue(field, itemValue)
-                                    }
-                                />
-                            </li>
-                        );
-                    })}
-            </ul>
-        );
-    };
 
     return (
         <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
@@ -117,25 +59,15 @@ const HeroSearchForm = () => {
                 />
             )}
 
-            <label className={styles.form__label}>
-                <IoIosPin className={styles.form__icon} />
-
-                <input
-                    className={styles.form__input}
-                    placeholder="location"
-                    {...register('locationInput')}
-                    aria-expanded={
-                        !!locationList?.length &&
-                        !!locationInput &&
-                        !isEquallyLocation
-                    }
-                />
-
-                {!!locationList?.length &&
-                    locationInput &&
-                    !isEquallyLocation &&
-                    renderDropDownItems(locationList)}
-            </label>
+            <LocationInput<IHeroSearchForm>
+                register={register}
+                setValue={setValue}
+                watch={watch}
+                customLabelClass={styles.form__label}
+                customInputClass={styles.form__input}
+                placeholder="job title, keywords, or company"
+                name="locationInput"
+            />
 
             <label className={styles['form__select-label']}>
                 <select className={styles.form__select} {...register('select')}>
