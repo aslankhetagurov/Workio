f0c55a1fc85ceb3101bbb6de1c1a63f003ba4acd
Aslan Khetagurov <aslankhetagurovv@gmail.com>
2025-06-27 22:59:51 +0300

    Add CompanyInfo to SingleVacancy component

--- src/modules/SingleVacancy/components/SingleVacancy/SingleVacancy.tsx
+++ src/modules/SingleVacancy/components/SingleVacancy/SingleVacancy.tsx
@@ -4,9 +4,9 @@ import { useGetVacancyQuery } from '../../api/singleVacancyApi';
 import SingleVacancyHeader from '../SingleVacancyHeader/SingleVacancyHeader';
 import ErrorComponent from '@/shared/UI/ErrorComponent/ErrorComponent';
 import SingleVacancyOverview from '../SingleVacancyOverview/SingleVacancyOverview';
-import SingleVacancyCompany from '../SingleVacancyCompany/SingleVacancyCompany';
 import SingleVacancyDescription from '../SingleVacancyDescription/SingleVacancyDescription';
 import Spinner from '@/shared/UI/Spinner/Spinner';
+import CompanyInfo from '@/shared/components/CompanyInfo/CompanyInfo';
 import styles from './SingleVacancy.module.scss';
 
 export const SingleVacancy = () => {
@@ -44,7 +44,7 @@ export const SingleVacancy = () => {
                     aria-label="Vacancy details"
                 >
                     <SingleVacancyOverview vacancyData={vacancyData} />
-                    <SingleVacancyCompany companyData={vacancyData.companies} />
+                    <CompanyInfo companyData={vacancyData.companies} />
                 </aside>
             </main>
         </section>
