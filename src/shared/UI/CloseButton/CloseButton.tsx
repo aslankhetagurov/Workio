7bfc1f704117ad70c1997b2ea87d56a0080a195c
Aslan Khetagurov <aslankhetagurovv@gmail.com>
2025-07-09 23:31:38 +0300

    Add size, customIconClass, ariaLabel and title props to CloseButton

--- src/shared/UI/CloseButton/CloseButton.tsx
+++ src/shared/UI/CloseButton/CloseButton.tsx
@@ -4,18 +4,34 @@ import styles from './CloseButton.module.scss';
 
 interface ICloseButtonProps {
     onClick: () => void;
+    size?: number;
+    customIconClass?: string;
+    ariaLabel?: string;
+    title?: string;
 }
 
-const CloseButton = ({ onClick }: ICloseButtonProps) => {
+const CloseButton = ({
+    onClick,
+    size = 26,
+    customIconClass,
+    ariaLabel,
+    title,
+}: ICloseButtonProps) => {
     return (
         <div className={styles['close-button__wrapper']}>
             <button
                 className={styles['close-button']}
                 onClick={onClick}
                 type="button"
-                aria-label="Close button"
+                aria-label={ariaLabel || 'Close button'}
+                title={title || ''}
             >
-                <FaWindowClose className={styles['close-button__icon']} />
+                <FaWindowClose
+                    className={`${styles['close-button__icon']} ${
+                        customIconClass ?? ''
+                    }`}
+                    style={{ fontSize: `${size}px` }}
+                />
             </button>
         </div>
     );
