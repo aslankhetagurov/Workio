39b8e704cd903e2fa8d777d7af8973596008f7c1
Aslan Khetagurov <aslankhetagurovv@gmail.com>
2025-08-14 12:05:15 +0300

    Add editableResume state to resumesSlice

--- src/modules/Resumes/store/resumesSlice.ts
+++ src/modules/Resumes/store/resumesSlice.ts
@@ -2,13 +2,16 @@ import { createSlice } from '@reduxjs/toolkit';
 
 import { RootState } from '@/store';
 import { IJobSearchForm } from '@/shared/components/JobSearchForm/JobSearchForm';
+import { ResumeWithExperiencesAndEducations } from '@/shared/types/database.types';
 
 interface IResumesSlice {
     resumesSearchFilters: IJobSearchForm | null;
+    editableResume: ResumeWithExperiencesAndEducations | null;
 }
 
 const initialState: IResumesSlice = {
     resumesSearchFilters: null,
+    editableResume: null,
 };
 
 const resumesSlice = createSlice({
@@ -18,13 +21,20 @@ const resumesSlice = createSlice({
         setResumesSearchFilters: (state, action) => {
             state.resumesSearchFilters = action.payload;
         },
+        setEditableResume: (state, action) => {
+            state.editableResume = action.payload;
+        },
     },
 });
 
 const { reducer, actions } = resumesSlice;
 
-export const { setResumesSearchFilters } = actions;
+export const { setResumesSearchFilters, setEditableResume } = actions;
+
 export const selectResumesSearchFilters = (state: RootState) =>
     state.resumesReducer.resumesSearchFilters;
+
+export const selectEditableResume = (state: RootState) =>
+    state.resumesReducer.editableResume;
 
 export const resumesReducer = reducer;
