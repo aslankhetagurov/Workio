2bea98620067f20971acd4df00f98d1c8436dd80
Aslan Khetagurov <aslankhetagurovv@gmail.com>
2025-08-19 00:22:33 +0300

    Add functionality to SingleVacancyHeader to send application on vacancy

--- src/modules/SingleVacancy/components/SingleVacancyHeader/SingleVacancyHeader.tsx
+++ src/modules/SingleVacancy/components/SingleVacancyHeader/SingleVacancyHeader.tsx
@@ -1,6 +1,10 @@
 import { toast } from 'sonner';
 import { GoHeartFill } from 'react-icons/go';
+import { FaCheck } from 'react-icons/fa6';
+import { useRef, useState } from 'react';
+
 import { VacancyWithCompany } from '@/shared/types/database.types';
+import PrimaryButton from '@/shared/UI/PrimaryButton/PrimaryButton';
 import logo from '@/shared/assets/images/company-logo.webp';
 import { useAppSelector } from '@/store/hooks';
 import { selectUserData } from '@/modules/Auth';
@@ -8,6 +12,13 @@ import {
     useGetFavoriteVacanciesQuery,
     useToggleFavoriteVacancyMutation,
 } from '@/modules/Vacancies';
+import { useGetApplicantResumesQuery } from '@/modules/Resumes';
+import DropDownList from '@/shared/UI/DropDownList/DropDownList';
+import {
+    useGetApplicationsQuery,
+    useSetApplicationMutation,
+} from '@/modules/Applications/api/applicationsApi';
+import { useCloseViaClickOutsideAndEsc } from '@/shared/hooks/useCloseViaClickOutsideAndEsc';
 import styles from './SingleVacancyHeader.module.scss';
 
 interface SingleVacancyHeaderProps {
@@ -21,11 +32,29 @@ const SingleVacancyHeader = ({ data }: SingleVacancyHeaderProps) => {
         user?.id ?? '',
         { skip: !user }
     );
+    const { data: resumes } = useGetApplicantResumesQuery(user?.id ?? '', {
+        skip: !user,
+    });
+    const { data: applications } = useGetApplicationsQuery(user?.id ?? '', {
+        skip: !user,
+    });
+    const [setApplication] = useSetApplicationMutation();
+    const [showResumes, setShowResumes] = useState(false);
+    const toggleShowResumes = () => setShowResumes((prev) => !prev);
+    const resumesRef = useRef(null);
 
-    if (!data || !user) return null;
+    useCloseViaClickOutsideAndEsc(resumesRef, showResumes, setShowResumes);
+
+    if (!data || !user || !resumes) return null;
 
     const isFavorite = favoriteVacancies?.some(
         (vacancy) => vacancy.vacancy_id === data.id
+    );
+
+    const isApplied = applications?.some(
+        (application) =>
+            application.vacancy_id === data.id &&
+            application.status === 'pending'
     );
 
     const handleToggleFavoriteVacancy = async () => {
@@ -47,6 +76,26 @@ const SingleVacancyHeader = ({ data }: SingleVacancyHeaderProps) => {
                     : 'Failed to toggle favorite status'
             );
         }
+    };
+
+    const transformResumesToOptions = () => {
+        return resumes.map((resume) => ({
+            label: resume.title,
+            value: resume.id,
+        }));
+    };
+
+    const handleSetApplication = async (resumeId: string) => {
+        const res = await setApplication({
+            user_id: user.id,
+            vacancy_id: data.id,
+            resume_id: resumeId,
+        });
+
+        toggleShowResumes();
+
+        if (res.data?.applicationId)
+            toast.success('Application send successfully');
     };
 
     return (
@@ -71,6 +120,35 @@ const SingleVacancyHeader = ({ data }: SingleVacancyHeaderProps) => {
 
             {user?.role === 'applicant' && (
                 <div className={styles.header__buttons}>
+                    <div className={styles.header__apply}>
+                        <PrimaryButton
+                            label={isApplied ? 'Applied' : 'Apply'}
+                            ariaLabel={` ${
+                                isApplied ? 'Applied' : 'Apply'
+                            } for a vacancy`}
+                            style={{ width: '200px' }}
+                            onClick={toggleShowResumes}
+                            icon={<FaCheck />}
+                            isShowIcon={isApplied}
+                            disabled={isApplied}
+                        />
+
+                        <div
+                            className={`${styles.header__resumes} ${
+                                showResumes && styles['header__resumes-show']
+                            } `}
+                            ref={resumesRef}
+                        >
+                            <p>Choose a Resume</p>
+                            <DropDownList
+                                list={transformResumesToOptions()}
+                                showDropDown={showResumes}
+                                handleSetValue={handleSetApplication}
+                                customClass={styles.header__dropdown}
+                            />
+                        </div>
+                    </div>
+
                     <button
                         className={
                             isFavorite
