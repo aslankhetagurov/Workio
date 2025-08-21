fd75dbe71f13fa6ed26aeaa92c6d5fda3fc209bf
Aslan Khetagurov <aslankhetagurovv@gmail.com>
2025-08-21 23:45:14 +0300

    Add setEditableCompany reducer to companiesSlice

--- src/modules/Companies/store/companiesSlice.ts
+++ src/modules/Companies/store/companiesSlice.ts
@@ -1,30 +1,44 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import { RootState } from '@/store';
 import { ICompanySearchForm } from '../components/CompaniesSearchForm/CompaniesSearchForm';
+import { CompanyWithUserAndVacanciesAndReviews } from '@/shared/types/database.types';
 
 interface ICompaniesSlice {
     companiesSearchFilters: ICompanySearchForm | null;
+    editableCompany: CompanyWithUserAndVacanciesAndReviews | null;
 }
 
 const initialState: ICompaniesSlice = {
     companiesSearchFilters: null,
+    editableCompany: null,
 };
 
 const companiesSlice = createSlice({
     name: 'companies',
     initialState,
     reducers: {
-        setCompaniesSearchFilters: (state, action) => {
+        setCompaniesSearchFilters: (
+            state,
+            action: PayloadAction<ICompanySearchForm | null>
+        ) => {
             state.companiesSearchFilters = action.payload;
+        },
+        setEditableCompany: (
+            state,
+            action: PayloadAction<CompanyWithUserAndVacanciesAndReviews | null>
+        ) => {
+            state.editableCompany = action.payload;
         },
     },
 });
 
 const { reducer, actions } = companiesSlice;
 
-export const { setCompaniesSearchFilters } = actions;
+export const { setCompaniesSearchFilters, setEditableCompany } = actions;
 export const selectCompaniesSearchFilters = (state: RootState) =>
     state.companiesReducer.companiesSearchFilters;
+export const selectEditableCompany = (state: RootState) =>
+    state.companiesReducer.editableCompany;
 
 export const companiesReducer = reducer;
